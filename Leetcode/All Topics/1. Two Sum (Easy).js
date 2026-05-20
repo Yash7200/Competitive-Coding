75ebeaa8a31691355edc1f8080af8a50865962f7
Yash Gandhi <yashgandhi7200@gmail.com>
2026-05-20 12:09:19 +0530

    add: Two Sum optimal solution

--- Leetcode/All Topics/1. Two Sum (Easy).js
+++ Leetcode/All Topics/1. Two Sum (Easy).js
@@ -1,35 +1,52 @@
 // solution - 1
 const twoSum = (nums, target) => {
-    const arrayLength = nums.length;
+  const arrayLength = nums.length;
 
-    for(let i = 0; i < arrayLength; i++){
-        for(let j = i+1; j< arrayLength; j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j];
-            }
-        }
+  for (let i = 0; i < arrayLength; i++) {
+    for (let j = i + 1; j < arrayLength; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
+  }
 };
 
 // solution - 2
 const twoSum = (nums, target) => {
-    let isValidTargetSum = false;
-    const validIndexes = [];
+  let isValidTargetSum = false;
+  const validIndexes = [];
+
+  nums.some((currentNum, currentIndex) => {
+    if (currentIndex === nums.length - 1) return true;
+
+    const restOfArray = nums.slice(currentIndex + 1, nums.length);
+
+    restOfArray.some((otherNum, otherIndex) => {
+      isValidTargetSum = currentNum + otherNum === target;
+      isValidTargetSum
+        ? validIndexes.push(currentIndex, currentIndex + otherIndex + 1)
+        : {};
+
+      return isValidTargetSum;
+    });
+
+    return isValidTargetSum;
+  });
+
+  return validIndexes;
+};
+
+// solution - 3
+const twoSum = (nums, target) => {
+  const arrLength = nums.length;
+
+  const numObj = {};
+  for(let i = 0; i < arrLength; ++i){
+    const component = target - nums[i];
     
-    nums.some((currentNum, currentIndex) => {
-        if(currentIndex === nums.length - 1) return true;
-        
-        const restOfArray = nums.slice(currentIndex + 1,nums.length);
-        
-        restOfArray.some((otherNum, otherIndex) => {
-            isValidTargetSum = currentNum + otherNum === target;
-            (isValidTargetSum) ? validIndexes.push(currentIndex, currentIndex+otherIndex+1) : {}
-            
-            return isValidTargetSum
-        })
-        
-        return isValidTargetSum;
-    })
-    
-    return validIndexes
+    if(numObj[component] !== undefined){
+        return [numObj[component], i];
+    }
+    numObj[nums[i]] = i;
+  }
 };
